d8f36178e9387fccc144aac636399c34b104e2e0
急急如律令 <kyukyunyorituryo@live.jp>
2023-07-22 16:53:55 +0900

    gatsby化

---
+++ src/pages/distribution.js
@@ -0,0 +1,33 @@
+// src/pages/index.js
+import React from "react"
+       const Named = () => (
+ <div>
+  <h1>青空文庫用ツール</h1>
+  <p>Meryの青空文庫用の構文ファイル、スニペットツール用のファイル。</p>
+  <p>
+    <a href="https://github.com/kyukyunyorituryo/aozoratool/raw/main/aozoraEditor.zip">
+      aozoraEditor.zip
+    </a>
+  </p>
+  <h2>青空文庫用整形補助</h2>
+  <p>青空文庫のタグつけを支援するマクロです．</p>
+  <p>
+    <a href="https://www.haijin-boys.com/wiki/%E9%9D%92%E7%A9%BA%E6%96%87%E5%BA%AB%E7%94%A8%E6%95%B4%E5%BD%A2%E8%A3%9C%E5%8A%A9">
+      https://www.haijin-boys.com/wiki/青空文庫用整形補助
+    </a>
+  </p>
+  <h2>説明ページ</h2>
+  <p>
+    <a href="https://99nyorituryo.hatenablog.com/entry/2013/10/28/231520">
+      Meryの青空文庫用整形補助マクロを使ってみる
+    </a>
+  </p>
+  <p>青空文庫シンタックスハイライトの構文ファイル</p>
+  <p>
+    <a href="https://www.haijin-boys.com/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:aozoramsy.zip">
+      https://www.haijin-boys.com/wiki/ファイル:aozoramsy.zip
+    </a>
+  </p>
+  </div>
+)
+export default Named
